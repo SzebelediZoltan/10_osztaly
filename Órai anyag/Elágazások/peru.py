aff0ea819c04e2740b2289eca59aeebf13119133
szebeledi.zoltan <szebeledi.zoltan@ckik.hu>
2022-10-10 08:22:26 +0200

    helyre

---
+++ Órai anyag/Elágazások/peru.py
@@ -0,0 +1,12 @@
+osztalyzat = int(input("Osztalyzat: "))
+
+if osztalyzat < 1 or osztalyzat > 20:
+    print("Hibas! (1<=osztalyzat>=20)")
+    exit() 
+else:
+    if osztalyzat <= 10:
+        print("Bukott!")
+    else:
+        print("Atment!")
+        
+print("Program vége!") 
